e90261589951459ff3811ab161bc090d9e3975d6
wiximix <wiximix@gmail.com>
2018-07-30 10:28:11 +0800

    项目初始化

---
+++ packages/index.js
@@ -0,0 +1,27 @@
+// import Button from './button'
+// import MessageBox from './messageBox'
+// import Dialog from './dialog'
+
+const components = [
+//   Button,
+//   MessageBox,
+//   Dialog
+]
+
+const install = (Vue = {}) => {
+  components.map(component => Vue.component(component.name, component))
+
+//   Vue.prototype.$alert = MessageBox.alert // eslint-disable-line no-param-reassign
+}
+
+// 自动安装
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export {
+  install,
+//   Button,
+//   MessageBox,
+//   Dialog
+}
