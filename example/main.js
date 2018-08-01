93fc060553a09e8f1c2b9a692655d1dc07b6e16e
WiX <wangxiang@babyeye.com>
2018-08-01 17:15:53 +0800

    移除 ts

--- example/main.js
+++ example/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueUwp from '../components'
 
 Vue.config.productionTip = false
+
+Vue.use(VueUwp)
+
+// import { Button } from '../components'
 
 new Vue({
   render: h => h(App)
