2d4468c4f9ea1c1be28968bc675170748ace8b6c
WiX <wangxiang@babyeye.com>
2018-08-07 23:53:14 +0800

    添加 大小写敏感

---
+++ components/icon/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+  <img v-if="a" :id="id" :src="name" :class="cls" :style="style" @click="eventClick" />
+  <span v-else :id="id" :src="name" :class="cls" :style="style" @click="eventClick"></span>
+</template>
+
+<script>
+import VueUwp from '../VueUwp'
+export default {
+  name: 'VupIcon',
+  props: {
+
+  }
+}
+</script>
+
---
+++ components/icon/icons/Notice.tsx
@@ -0,0 +1,1 @@
+export const iconNotice: string = `<symbol id="iconNotice" viewBox="0 0 16 16"><rect x="0" fill="none" width="16" height="16"/><path d="M8 2C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6-2.7-6-6-6zm0 10c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm.8-3H7.2L7 4h2l-.2 5z"/></symbol>`
